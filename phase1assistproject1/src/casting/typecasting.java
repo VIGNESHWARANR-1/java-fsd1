f7f74b81928a32e2301f3ee00cd5a1064aec1a2c
VIGNESHWARAN R <vickychella27@gmail.com>
2022-07-26 12:18:13 +0530

    myassignment

---
+++ phase1assistproject1/src/casting/typecasting.java
@@ -0,0 +1,20 @@
+package casting;
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int i = 20;
+	      long l = i;
+	      float f = l;
+	      double d = f;
+	      System.out.println("Int value " + i);
+	      System.out.println("After type casting:");
+	      System.out.println("Int to Long value " + l);
+	      System.out.println("Int to Float value " + f);
+	      System.out.println("Int to Double value " + d);
+
+
+	}
+
+}
