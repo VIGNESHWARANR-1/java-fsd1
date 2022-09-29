2140ce409698bb1a934ef7e013146d5799869412
VIGNESHWARAN R <109645706+VIGNESHWARANR-1@users.noreply.github.com>
2022-09-29 15:52:19 +0530

    myassignment

---
+++ PHASE3-USERFEEDBACK/src/main/java/com/example/userfeedback/Phase3UserfeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.userfeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3UserfeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3UserfeedbackApplication.class, args);
+	}
+
+}
---
+++ PHASE3-final project online quiz/src/main/java/com/main/repository/ResultRepo.java
@@ -0,0 +1,11 @@
+package com.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.main.model.Result;
+
+@Repository
+public interface ResultRepo extends JpaRepository<Result, Integer> {
+	
+}
